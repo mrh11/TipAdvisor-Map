c20a1af7540dde688616e664bf465fd9fc2b7d41
Raihan Hossain <raihanhossain0@gmail.com>
2019-05-31 12:03:23 -0400

    Add files via upload

---
+++ db/index.js
@@ -0,0 +1,20 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database('/Users/Raihan/Desktop/FEClocationService/db/hotels', sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.log("Could not connect to Database: ", err);
+  }
+  console.log("Connected to the database.");
+});
+
+var getInfoById = (obj, callback) => {
+
+      db.all("SELECT * FROM data where id=(?)", [obj.hotelID],(err, result) => {
+         callback(err, result); 
+      });
+}
+
+module.exports = {
+  getInfoById
+}
+
